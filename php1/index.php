95c5c4f429e6f002b752f11ef0fc52d606bbf2b5
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-04-06 11:48:33 +0530

    add some changes

--- php1/index.php
+++ php1/index.php
@@ -6,8 +6,45 @@
     <title>Document</title>
 </head>
 <body>
-    <?php  
-      echo " helo php " ?> 
+     <!-- <?php 
+        // echo "Ajeet kumar";
 
+           $name = "Ajeetkumar";
+           $age = 23;
+           $marks = 45.4;
+           $isMaried = true ;
+           $nam = "Ajay kumamr ";
+
+           
+    echo "My name is " . $name . "<br/>"; // Corrected the line break syntax
+    echo "My age is " . $age  . "<br/>" ;
+    echo " MY marks is "  . $marks   ; 
+    echo " <br/> My name is  $nam  % " ; 
+    echo "  are you married " . $isMaried ; 
+
+     var_dump($name);  
+     var_dump($age) ; 
+     var_dump($marks) ;  
+     ?> -->
+
+
+     <?php  
+        // $a = 10;
+        // $b = 20 ; 
+        //  $c = 2 ;
+
+        //   echo " total of number = "  .  $a + $b  . "<br/>" ; 
+
+        //   echo $a - $b ; 
+        //   echo " <br/>  a and c total of  = " . $a * $c ; 
+
+         $a = 12;
+          
+         echo $a  . "<br/>" ;
+          $a++ ; 
+         echo $a . "<br/>" ;
+          $a-- ;  
+         echo $a ; 
+     ?>
 </body>
 </html>
