0f76f0841b6a6ade01c012213596352964dee79c
AjeetKumarYadav07 <iamsupertrader07@gmail.com>
2024-05-04 17:15:55 +0530

    add some changes

--- PHPWeb/index.php
+++ PHPWeb/index.php
@@ -42,12 +42,24 @@
            <form action= "some_file.php" mehtod="post">
 
     */
+      //  echo "<pre>"; 
+    
+      if (isset($_POST["submit"])) {
+          if (empty($_POST["username"])) {
+              echo "Username is empty";
+          } else {
+              echo "Username is not empty";
+          }
+      }
+
+      
+      //  print_r($_GET); 
 
 
       
       ?>
 
-      <form action = "index.php" mehtod= "get" >
+      <form action = "index.php" method= "post" >
           <div>
              <label for=""> Username:</label>
              <input type="text" name= "username" >
@@ -58,7 +70,91 @@
                 <label for="">Password:</label>
                 <input type="password" name ="password" >
             </div>
-             <button type="submit" >  Log in  </button>
+             <button type="submit"  name = "submit" >  Log in  </button>
         </form>
+
+
+
+        <br> <br> <br>
+
+        <a href="./index.php " > Home </a>;
+
+
+
+
+
+
+
+
+
+        <form action="index.php" method="post">
+    <label>Select your course:</label> <br> 
+
+    <input type="radio" value="BCA" name="course"> BCA <br>
+    <input type="radio" value="MCA" name="course"> MCA <br>
+    <input type="radio" value="Same" name="course"> Same <br>
+    <input type="radio" value="BA" name="course"> BA <br>
+    <input type="radio" value="BBA" name="course"> BBA <br>
+    <input type="radio" value="B.Tech" name="course"> B.TECH <br>
+
+    <input type="submit" name="submit" value="Confirm">
+</form>
+
+
+
+
+
+        <?php 
+if (isset($_POST["submit"])){
+    $output = $_POST['course'];
+
+    switch ($output) {
+        case 'BCA':
+            echo "You selected BCA";
+            break;
+        
+        case "Same":
+            echo "You selected Same";
+            break;
+        
+        case "MCA":
+            echo "You selected MCA";
+            break;
+        
+        case "BBA":
+            echo "You selected BBA";
+            break;
+
+        default:
+            echo "Please select something!";
+    }
+}
+?>
+
+
+
+
+
+
+
+  <!-- // checkbox in PHP  -->
+
+     <form action="" method="post" >
+        
+       <label for=""> Please slecte your fav. dishes </label> <br> <br>
+        <input type="checkbox" value="Apple" name="fruit" > Apple  <br>
+        <input type="checkbox" value="Banna" name="fruit" > Banna  <br>
+        <input type="checkbox" value="Chiku" name="fruit" > Chiku <br>
+        <input type="checkbox" value="Graps" name="fruit" > Graps <br>
+        <input type="checkbox" value="Mango" name="fruit" > Mango <br>
+      
+          <input type="submit"  value="submit" name="sumit" >
+     
+     </form>
+   
+
+        
+    
+
 </body>
 </html>
